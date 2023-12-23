86fb78ed3ebe1d6954f5f320b9657691d74037ca
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-23 23:03:23 +0530

    Added Error handler Middleware

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,23 @@ const exprees = require("express");
 const dotenv = require("dotenv");
 const ConnectDB = require("./config/db.js");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes.js");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 dotenv.config({});
 const app = new exprees();
 
-const { chats } = require("./data/chat");
-
 app.use(cors());
-
+app.use(exprees.json());
 //Connecting To DB
 ConnectDB();
 
-app.get("/api/v1/", (req, res) => {
-  res.send("Chit-Chat API Running Successfully!!!");
-});
-app.get("/api/v1/chat", (req, res) => {
-  res.send(chats);
-});
+//mounting Route
+app.use("/api/v1/user", userRoutes);
+
+//Error Middleware
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server Running at http://localhost: ${PORT}`);
