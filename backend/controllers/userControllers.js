51024c287f78d88e83bd5340ff41bf7032f381e2
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-23 23:06:11 +0530

    Added login endpoint

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -38,3 +38,23 @@ exports.registerUser = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
+
+exports.authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
