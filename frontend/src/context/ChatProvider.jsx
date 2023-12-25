093f371133bb4f9273d027642066cc13b0d2612b
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-26 00:07:19 +0530

    Added new State to context for SelectedChat and setChats

--- frontend/src/context/ChatProvider.jsx
+++ frontend/src/context/ChatProvider.jsx
@@ -4,7 +4,9 @@ import { useHistory } from "react-router-dom";
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
+  const [selectedChat, setSelectedChat] = useState();
   const [user, setUser] = useState();
+  const [chats, setChats] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
@@ -17,7 +19,9 @@ const ChatProvider = ({ children }) => {
     } */
   }, [history]);
   return (
-    <ChatContext.Provider value={{ user, setUser }}>
+    <ChatContext.Provider
+      value={{ user, setUser, chats, setChats, selectedChat, setSelectedChat }}
+    >
       {children}
     </ChatContext.Provider>
   );
