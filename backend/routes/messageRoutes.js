465479451df8ea7c07df3d9252eda3170c4c325a
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-26 17:49:01 +0530

    Added Send Message API endpoint

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
+const { sendMessage } = require("../controllers/messageControllers");
+
+const router = express.Router();
+
+router.post("/", protect, sendMessage);
+module.exports = router;
