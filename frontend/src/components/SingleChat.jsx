f1f3245ab11840404482a49376f993fd12406473
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-26 19:45:25 +0530

    Added Send Messages UI

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -1,11 +1,25 @@
-import { Box, IconButton, Text } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  FormControl,
+  IconButton,
+  Input,
+  Spinner,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import { ChatState } from "../context/ChatProvider";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { getSenderFull, getSender } from "../config/ChatLogics";
 import ProfileModel from "./ProfileModel";
 import UpdateGroupChatModal from "./UpdateGroupChatModal";
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  const typingHandler = () => {};
+  const sendMessage = () => {};
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newMessage, setNewMessage] = useState("");
+  const toast = useToast();
   const { selectedChat, setSelectedChat, user } = ChatState();
   return (
     <>
@@ -51,12 +65,36 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             h="100%"
             borderRadius="lg"
             overflowY="hidden"
-          ></Box>
+          >
+            {loading ? (
+              <Spinner
+                size="xl"
+                w={20}
+                h={20}
+                alignSelf="center"
+                margin="auto"
+              />
+            ) : (
+              <></>
+            )}
 
-          {/* All Message Here */}
+            <FormControl
+              onKeyDown={sendMessage}
+              id="first-name"
+              isRequired
+              mt={3}
+            >
+              <Input
+                variant="filled"
+                bg="#E0E0E0"
+                placeholder="Enter a message.."
+                value={newMessage}
+                onChange={typingHandler}
+              />
+            </FormControl>
+          </Box>
         </>
       ) : (
-        // to get socket.io on same page
         <Box
           display="flex"
           alignItems="center"
