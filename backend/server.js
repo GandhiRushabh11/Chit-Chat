798095c54154e9e09d01d88111c792fa57a3c37f
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-30 18:18:18 +0530

    Updated for live server

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,25 @@ app.get("/swagger", swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/chat", chatRoutes);
 app.use("/api/v1/message", messageRoutes);
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+console.log(__dirname1);
+if (process.env.NODE_ENV === "production") {
+  app.use(exprees.static(path.join(__dirname1, "..", "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname1, "..", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 //Error Middleware
 app.use(notFound);
 app.use(errorHandler);
